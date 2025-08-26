0d7fbf494d1d1297e7ac3a6a497cc44d82571529
David Kocher <dkocher@iterate.ch>
2025-08-26 20:49:52 +0200

    Delete unused.

--- azure/src/test/java/ch/cyberduck/core/cryptomator/CopyWorkerTest.java
+++ azure/src/test/java/ch/cyberduck/core/cryptomator/CopyWorkerTest.java
@@ -23,7 +23,6 @@ import ch.cyberduck.core.DisabledProgressListener;
 import ch.cyberduck.core.Path;
 import ch.cyberduck.core.PathCache;
 import ch.cyberduck.core.azure.AbstractAzureTest;
-import ch.cyberduck.core.azure.AzureDeleteFeature;
 import ch.cyberduck.core.azure.AzureDirectoryFeature;
 import ch.cyberduck.core.azure.AzureFindFeature;
 import ch.cyberduck.core.azure.AzureReadFeature;
@@ -81,7 +80,7 @@ public class CopyWorkerTest extends AbstractAzureTest {
         session.withRegistry(registry);
         final byte[] content = RandomUtils.nextBytes(40500);
         final TransferStatus status = new TransferStatus();
-        new CryptoBulkFeature<>(session, new DisabledBulkFeature(), new AzureDeleteFeature(session, null), cryptomator).pre(Transfer.Type.upload, Collections.singletonMap(new TransferItem(source), status), new DisabledConnectionCallback());
+        new CryptoBulkFeature<>(session, new DisabledBulkFeature(), cryptomator).pre(Transfer.Type.upload, Collections.singletonMap(new TransferItem(source), status), new DisabledConnectionCallback());
         new StreamCopier(new TransferStatus(), new TransferStatus()).transfer(new ByteArrayInputStream(content), new CryptoWriteFeature<>(session, new AzureWriteFeature(session, null), cryptomator).write(source, status.setLength(content.length), new DisabledConnectionCallback()));
         assertTrue(cryptomator.getFeature(session, Find.class, new DefaultFindFeature(session)).find(source));
         final CopyWorker worker = new CopyWorker(Collections.singletonMap(source, target), new SessionPool.SingleSessionPool(session, registry), PathCache.empty(), new DisabledProgressListener(), new DisabledConnectionCallback());
--- cryptomator/src/main/java/ch/cyberduck/core/cryptomator/features/CryptoBulkFeature.java
+++ cryptomator/src/main/java/ch/cyberduck/core/cryptomator/features/CryptoBulkFeature.java
@@ -18,15 +18,12 @@ package ch.cyberduck.core.cryptomator.features;
 import ch.cyberduck.core.ConnectionCallback;
 import ch.cyberduck.core.Local;
 import ch.cyberduck.core.Path;
-import ch.cyberduck.core.RandomStringService;
 import ch.cyberduck.core.Session;
-import ch.cyberduck.core.UUIDRandomStringService;
 import ch.cyberduck.core.cryptomator.AbstractVault;
 import ch.cyberduck.core.cryptomator.random.RandomNonceGenerator;
 import ch.cyberduck.core.cryptomator.random.RotatingNonceGenerator;
 import ch.cyberduck.core.exception.BackgroundException;
 import ch.cyberduck.core.features.Bulk;
-import ch.cyberduck.core.features.Delete;
 import ch.cyberduck.core.transfer.Transfer;
 import ch.cyberduck.core.transfer.TransferItem;
 import ch.cyberduck.core.transfer.TransferStatus;
@@ -41,16 +38,13 @@ import java.util.Map;
 
 public class CryptoBulkFeature<R> implements Bulk<R> {
 
-    private final RandomStringService random
-            = new UUIDRandomStringService();
-
     private final Session<?> session;
     private final Bulk<R> delegate;
     private final AbstractVault cryptomator;
 
-    public CryptoBulkFeature(final Session<?> session, final Bulk<R> delegate, final Delete delete, final AbstractVault cryptomator) {
+    public CryptoBulkFeature(final Session<?> session, final Bulk<R> delegate, final AbstractVault cryptomator) {
         this.session = session;
-        this.delegate = delegate.withDelete(cryptomator.getFeature(session, Delete.class, delete));
+        this.delegate = delegate;
         this.cryptomator = cryptomator;
     }
 
@@ -99,12 +93,6 @@ public class CryptoBulkFeature<R> implements Bulk<R> {
             }
         }
         return delegate.pre(type, encrypted, callback);
-    }
-
-    @Override
-    public Bulk<R> withDelete(final Delete delete) {
-        delegate.withDelete(cryptomator.getFeature(session, Delete.class, delete));
-        return this;
     }
 
     @Override
